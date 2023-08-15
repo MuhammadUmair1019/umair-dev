e2ba8393e5f004f1277c7ccd5a87a23c2119f925
Muhammad Umair <Muhammad.umair1019@gmail.com>
2023-08-16 02:41:36 +0500

    First Version

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`
+Hi there I am Muhammad Umair , Full Stack Developer from Pakisan 🇵🇰
+
+✔️ Full Stack Developer
+
+Github => 🔗 https://github.com/MuhammadUmair1019/
+LinkedIn: => 🔗 https://www.linkedin.com/in/muhammad-umair-a6792b199
+Youtube: => 🔗 https://www.youtube.com/c/UmairtipsStudio/
+`)
