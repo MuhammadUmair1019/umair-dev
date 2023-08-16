71bcedfd00e3913b4cf9726a101816bfae5b1b21
Muhammad Umair <Muhammad.umair1019@gmail.com>
2023-08-16 22:18:45 +0500

    Add welcome header

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
+const pkgJSON = require('./package.json')
+const welcome = require('cli-welcome')
+
+welcome({
+  title: pkgJSON.name,
+  tagLine: `Howdy, ncie to meet ya!`,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: false,
+})
+
 console.log(`
 Hi there I am Muhammad Umair , Full Stack Developer from Pakisan 🇵🇰
 
